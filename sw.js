2a968719017cf380a003fc5500ac1b39104a1396
laidbackkiwi-ai <kickme2940@gmail.com>
2026-07-21 21:08:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fujirock-companion-v5-1';
+const CACHE = 'fujirock-companion-v6';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
